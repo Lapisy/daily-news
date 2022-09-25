854bab80f2b60856b6992dab77a4ca49c36a163e
lapisy <lapisy@lapisydeMacBook-Pro.local>
2022-09-25 09:05:24 +0800

    add exception handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const {PORT} = require("./config/config.default")
 const morgan = require("morgan")
 const router = require("./router/index")
+const errorHandler = require('./middleware/error-handler')
 
 /**
  * 导入中间件
@@ -13,6 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded())
 
 app.use("/api",router)
+// add error handler
+app.use(errorHandler())
 
 app.listen(PORT,()=>{
     console.log(`Server is runnning at http://localhost:${PORT}`)
--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,5 +1,10 @@
 const express = require("express")
 
-exports.login = async (req, res,next) => {
-    res.send("/login====")
+exports.login = async (req, res, next) => {
+    try {
+        throw 'name is valid.'
+        res.send("/login====")
+    }catch (e) {
+        next(e)
+    }
 }
